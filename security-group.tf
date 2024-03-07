d3f6ad833b0114c59a6adcb51e3340f555d0152e
Jonathan Vinci <vincij002@gmail.com>
2024-03-07 15:04:15 -0500

    created security groups

--- security-group.tf
+++ security-group.tf
@@ -1,1 +1,127 @@
 # create security group for the application load balancer
+resource "aws_security_group" "alb_security_group" {
+  name        = "alb security group"
+  description = "enable http/https access on port 80/443"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "http access"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "https access"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb security grouo"
+  }
+}
+
+# create security group for SSH 
+resource "aws_security_group" "ssh_security_group" {
+  name        = "ssh security group"
+  description = "enable ssh access on port 22"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "ssh access"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.ssh_location]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ssh security group"
+  }
+}
+
+# Create Web Server Security Group
+resource "aws_security_group" "web_server_security_group" {
+  name        = "web server security group"
+  description = "enable http/https access on port 80/443 via alb sg and access on port 22 via ssh sg"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "http access"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    security_groups = [aws_security_group.alb_security_group.id]
+  }
+
+  ingress {
+    description = "https access"
+    from_port = 443
+    to_port = 443
+    protocol = "tcp"
+    security_groups = [aws_security_group.alb_security_group.id]
+  }
+
+   ingress {
+    description = "ssh access"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    security_groups = [aws_security_group.ssh_security_group.id]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web server security grouo"
+  }
+}
+
+# create data base security group
+resource "aws_security_group" "data_base_security_group" {
+  name        = "database security group"
+  description = "enable mysql/aurora access on port 3306"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "mysql/aurora access"
+    from_port = 3306
+    to_port = 3306
+    protocol = "tcp"
+    security_groups = [aws_security_group.web_server_security_group.id]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "database security group"
+  }
+}
