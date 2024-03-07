c353616c07702f5e24aab7a66a5d19f28fcd09b1
Jonathan Vinci <vincij002@gmail.com>
2024-03-06 21:06:43 -0500

    created vpc

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 # stores the terraform state file in S3
 terraform {
   backend "s3" {
-    bucket = "gela-terraform-remote-state"
-    key    = "terraform.tfstate.dev"
-    region = "us-east-1"
+    bucket  = "gela-terraform-remote-state"
+    key     = "terraform.tfstate.dev"
+    region  = "us-east-1"
     profile = "terraform-user"
-  }
+    }
 }
--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region  = "us-east-1"
   profile = "terraform-user"
 }
 
